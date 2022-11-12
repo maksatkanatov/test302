cf656c43c568cb91707e539543045e9223365c3d
Max <maksatkanat@gmail.com>
2022-11-12 10:49:13 +0600

    initial springboot commit

---
+++ src/test/java/kz/narxoz/carsshop/CarsShopApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.narxoz.carsshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
